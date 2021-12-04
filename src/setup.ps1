45a355d28ee551eb57e44a6322aade1e04379909
shiomiyan <dev.umisk@gmail.com>
2021-12-04 21:24:25 +0900

    fix conflict

--- src/setup.ps1
+++ src/setup.ps1
@@ -4,20 +4,23 @@ Set-ExecutionPolicy Bypass -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco feature enable -n allowGlobalConfirmation
+
 # install tools using choco
 cinst vim git fnm starship
+
 # install Rust
 Invoke-WebRequest "https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe" `
   -OutFile C:\rustup-init.exe
 C:\rustup-init.exe -y
+
 # reload env vars
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") `
   + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 refreshenv
 
+# clone repo
 git clone https://github.com/shiomiyan/.dotfiles.git $HOME/.dotfiles
 
 # === setup tools ===
@@ -29,12 +32,6 @@ echo "Invoke-Expression (&starship init powershell)" >> $profile
 # プロファイル再読み込みでエラーを吐くのでここでは一時的に読み込ませる
 fnm env --use-on-cd | Out-String | Invoke-Expression > $null
 echo "fnm env --use-on-cd | Out-String | Invoke-Expression" >> $profile *> $null
-
-# install node for coc-vim
-## install node version manager
-cinst fnm
-Write-Output "fnm env --use-on-cd | Out-String | Invoke-Expression" | Out-File -Encoding default -Append $profile
-& $profile
 fnm install v16.13.0
 fnm use v16.13.0
 
@@ -47,4 +44,3 @@ iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
 
 vim -es -u _vimrc -i NONE -c "PlugInstall" -c "qa"
-
