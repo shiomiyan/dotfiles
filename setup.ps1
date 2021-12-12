00408b43c5dce99fa982701661e476c4688a770f
shiomiyan <rhpav7@protonmail.com>
2021-12-12 21:44:46 +0900

    add install win32yank process

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,13 @@
 Set-ExecutionPolicy Bypass -Force
 
+# download win32yank
+Invoke-WebRequest `
+    -Uri "https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-x64.zip" `
+    -OutFile C:\win32yank.zip
+# unzip win32yank and remove tmp file
+Expand-Archive C:\win32yank.zip -DestinationPath C:\Tools\win32yank
+Remove-Item C:\win32yank.zip
+
 # install chocolatey
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
