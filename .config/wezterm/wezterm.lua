637465d4e8f2b5b382a1e35e5d87b188032dbcf7
shiomiyan <dev.umisk@gmail.com>
2022-03-20 16:16:36 +0900

    add settings by OS

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,35 @@
 local wezterm = require 'wezterm';
 
-return {
-  color_scheme = "Gruvbox Dark";
-  window_background_opacity = 0.9;
-  tab_bar_at_bottom = true;
+local shell;
+local font_profile;
 
-  use_fancy_tab_bar =false;
+if os.getenv('windir') then
+  shell = { 'pwsh.exe', '-NoLogo' }
+  font_profile = wezterm.font_with_fallback({
+    "Consolas",
+    "Rounded Mplus 1c"
+  })
+else
+  shell = { 'zsh', '--login' }
+  font_profile = wezterm.font("SF Mono Square")
+end
+
+return {
+  color_scheme = 'Gruvbox Dark',
+  window_background_opacity = 0.9,
+  tab_bar_at_bottom = true,
+
+  use_fancy_tab_bar =false,
 
   -- 起動時のウィンドウサイズ (文字数)
-  initial_rows = 25;
-  initial_cols = 100;
+  initial_rows = 25,
+  initial_cols = 100,
 
   -- font
-  font = wezterm.font_with_fallback({
-    "M+ 1mn",
-    "FirgeNerd Console"
-  });
-  font_size = 12;
-  line_height = 0.8;
-  use_ime = true;
+  font = font_profile,
+  font_size = 18,
+  line_height = 1.0,
+  use_ime = true,
 
-  default_prog = { 'pwsh.exe', '-NoLogo' };
+  default_prog = shell,
 }
