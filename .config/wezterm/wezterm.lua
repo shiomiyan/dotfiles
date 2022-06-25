d10a6e783317881c92a3bb65fe2cf9f9cd91490c
shiomiyan <sk736b@protonmail.com>
2022-06-25 20:21:18 +0900

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,53 +1,48 @@
 local wezterm = require 'wezterm';
 
-local shell;
-local font;
-local font_size;
+local config = {
+    color_scheme = "Gruvbox Dark",
+    window_background_opacity = 0.95,
+    tab_bar_at_bottom = true,
+    use_fancy_tab_bar = false,
+    use_ime = true,
 
-if os.getenv('windir') then
-  shell = { 'pwsh.exe', '-NoLogo' }
-  font = wezterm.font_with_fallback({
-    "Consolas",
-    -- "Rounded Mplus 1c"
-    "UDEV Gothic NF"
-  })
-  font_size = 13
+    -- 起動時のウィンドウサイズ (文字数)
+    initial_rows = 28,
+    initial_cols = 100,
+
+    -- pane appearance
+    inactive_pane_hsb = {
+        saturation = 0.8,
+        brightness = 0.4,
+    },
+
+    -- Key bindings
+    leader  = { key = "b", mods = "CTRL" },
+    keys    = {
+        { key = "w", mods = "LEADER", action = "ShowTabNavigator" },
+        { key = "c", mods = "LEADER", action = wezterm.action { SpawnTab = "CurrentPaneDomain" }},
+        { key = "x", mods = "LEADER", action = wezterm.action { CloseCurrentTab = { confirm = true }}},
+        { key = "h", mods = "LEADER", action = wezterm.action { SplitHorizontal = { domain= "CurrentPaneDomain" }}},
+        { key = "C", mods = "CTRL|SHIFT", action = "Copy" },
+    }
+}
+
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.shell = { 'pwsh.exe', '-NoLogo' }
+    config.font = wezterm.font_with_fallback({
+        "Consolas",
+        "Cica"
+    })
+    config.font_size = 13
+elseif wezterm.target_triple == "x86_64-apple-darwin" then
+    config.shell = { 'zsh', '--login' }
+    config.font = wezterm.font("SF Mono Square")
+    config.font_size = 22
 else
-  shell = { 'zsh', '--login' }
-  font = wezterm.font("SF Mono Square")
-  font_size = 22
+    config.shell = { 'zsh', '--login' }
 end
 
-return {
-  color_scheme = 'Gruvbox Dark',
-  window_background_opacity = 0.95,
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar =false,
 
-  -- 起動時のウィンドウサイズ (文字数)
-  initial_rows = 28,
-  initial_cols = 100,
-
-  -- font
-  font = font,
-  font_size = font_size,
-  line_height = 1.0,
-  use_ime = true,
-
-  default_prog = shell,
-
-  -- pane appearance
-  inactive_pane_hsb = {
-    saturation = 0.8,
-    brightness = 0.4,
-  },
-
-  -- Key bindings
-  leader  = { key = "b", mods = "CTRL" },
-  keys    = {
-    { key = "w", mods = "LEADER", action = "ShowTabNavigator" },
-    { key = "c", mods = "LEADER", action = wezterm.action { SpawnTab = "CurrentPaneDomain" }},
-    { key = "x", mods = "LEADER", action = wezterm.action { CloseCurrentTab = { confirm = true }}},
-    { key = "h", mods = "LEADER", action = wezterm.action { SplitHorizontal = { domain= "CurrentPaneDomain" }}},
-  }
-}
+return config
