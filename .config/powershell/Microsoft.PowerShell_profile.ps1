ac6fdb876986d10c3719b46fa280ddb6005b0584
shiomiyan <sk736b@protonmail.com>
2022-07-11 20:08:28 +0900

    Update PowerShell profile.

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -16,7 +16,22 @@ Function which {
     Get-Command -ShowCommandInfo $args | Format-Table -Property Definition
 }
 
+Function wsl-feature {
+    param (
+        $condition
+    )
+
+    if ($condition -eq "enable") {
+        dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+        dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    } elseif ($condition -eq "disable") {
+        Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux
+        Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform
+    }
+}
+
 Set-Alias -Name e -Value explorer.exe
 Set-Alias -Name vi -Value vim
 Set-Alias -Name wg -Value winget
 Set-Alias -Name tig -Value "C:\Program Files\Git\usr\bin\tig.exe"
+Set-Alias -Name z -Value zoxide.exe
