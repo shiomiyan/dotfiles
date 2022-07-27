c3da65dc5a444ea46e7ccf13841e2ef486da99b4
shiomiyan <sk736b@protonmail.com>
2022-07-27 21:06:50 +0900

    update setup script

--- setup/windows/Setup.ps1
+++ setup/windows/Setup.ps1
@@ -1,78 +1,81 @@
-# allow script execution
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Allow script execution
 Set-ExecutionPolicy Bypass -Force
 
-function Install-Apps {
+function Install-App {
     <#
 
 .DESCRIPTION
-    function for install require applications and dependencies
-    NOTE: you need to install winget manually
+    Function for install require applications and dependencies
 
 #>
 
-    # install winget if not exists
+    # Install winget if not exists
     if (-not (Get-Command winget -ea SilentlyContinue)) {
         # install dependencies for winget
-        Invoke-WebRequest `
-            -Uri "https://globalcdn.nuget.org/packages/microsoft.ui.xaml.2.7.1.nupkg" `
-            -OutFile "C:\Windows\Temp\microsoft.ui.xaml.2.7.1.zip"
-        Expand-Archive `
-            -Path "C:\Windows\Temp\microsoft.ui.xaml.2.7.1.zip" `
-            -DestinationPath "C:\Windows\Temp\microsoft.ui.xaml.2.7.1"
-        Add-AppxPackage "C:\Windows\Temp\microsoft.ui.xaml.2.7.1\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
-
-        Invoke-WebRequest `
-            -Uri "https://download.microsoft.com/download/4/7/c/47c6134b-d61f-4024-83bd-b9c9ea951c25/14.0.30035.0-Desktop/Microsoft.VCLibs.x64.14.00.Desktop.appx" `
-            -OutFile "C:\Windows\Temp\vclibs.appx"
-        Add-AppPackage "C:\Windows\Temp\vclibs.appx"
-
-        Install-Module PowershellGet -Force
-
-        Invoke-WebRequest `
-            -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" `
-            -OutFile "C:\Windows\Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        $wingetUrl = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Invoke-WebRequest -Uri $wingetUrl -OutFile "C:\Windows\Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
         Import-Module Appx -UseWindowsPowerShell
-        Add-AppxPackage C:\Windows\Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+        try {
+            Add-AppxPackage C:\Windows\Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+        } catch {
+            Write-Output "Failed to install winget. Try install manually." $wingetUrl
+        }
     }
 
-    # install chocolatey if not exists
+    # Install chocolatey if not exists
     if (-not (Get-Command choco -ea SilentlyContinue)) {
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
 
-    # install git
-    winget install git.git
+    # Install scoop if not exists
+    if (-not (Get-Command scoop -ea SilentlyContinue)) {
+        Invoke-RestMethod get.scoop.sh | Invoke-Expression
+    }
 
-    # refresh environment value
+    # Install git first
+    scoop install git
+
+    # Refresh environment value
     Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
     refreshenv
 
-    # clone repo if not exists
-    if ((Test-Path "$HOME\dotfiles") -ne "True") {
+    # Clone repo if not exists
+    if (!(Test-Path "$HOME\dotfiles")) {
         git clone https://github.com/shiomiyan/dotfiles.git $HOME/dotfiles
     }
 
-    # install applications from winget
+    # GUI applications are installed by winget
     winget import -i "$HOME\dotfiles\install_scripts\winget.json"
 
-    # always enable -y option
+    # Install runtimes, commandline tools
+    scoop install     `
+        make          `
+        git           `
+        gh            `
+        ghq           `
+        peco          `
+        sudo          `
+        deno          `
+        nodejs-lts    `
+        starship      `
+        bat           `
+        lsd           `
+        zoxide        `
+        fd            `
+        ripgrep       `
+        rga           `
+        hugo-extended
+
+    # Always enable -y option
     choco feature enable -n allowGlobalConfirmation
 
-    # install applications using choco
-    choco install     `
-        neovim        `
-        deno          `
-        zig           `
-        hugo-extended `
-        ripgrep       `
-        starship
+    # Install applications using choco
+    choco install neovim --pre
 
-    # Install Neovim vim-plug
-    Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-        New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
-
-    # install Rust
+    # Install Rust
     Invoke-WebRequest `
         -Uri "https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe" `
         -OutFile C:\rustup-init.exe
@@ -83,7 +86,7 @@ function Install-Apps {
 
 }
 
-function Connect-Dotfiles {
+function Create-Symlink {
 
     <#
 
@@ -103,7 +106,7 @@ function Connect-Dotfiles {
     New-Item -ItemType SymbolicLink `
         -Path   $env:LOCALAPPDATA\nvim `
         -Target $HOME\dotfiles\.config\nvim\
-    
+
     New-item -ItemType SymbolicLink `
         -Path   $env:APPDATA\espanso `
         -Target $HOME\dotfiles\.config\espanso
