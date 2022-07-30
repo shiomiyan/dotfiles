f101eac1a1724207a77d81a0a852d1bcd538ddf0
shiomiyan <sk736b@protonmail.com>
2022-07-31 00:07:02 +0900

    update powershell profile

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -21,27 +21,42 @@ function local:which {
     Get-Command -ShowCommandInfo $args | Format-Table -Property Definition
 }
 
-function local:wsl-feature {
-    param (
-        $condition
+function local:Switch-WslFeature {
+    Param (
+        [Parameter(Mandatory=$True)]
+        [String]$status
     )
 
-    if ($condition -eq "enable") {
-        dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-        dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+    $command = ""
+    if ($status -eq "enable") {
+        $command = "`
+            dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart; `
+            dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart"
+        $color = "Green"
+    } elseif ($status -eq "disable") {
+        $command = "`
+            Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux; `
+            Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform"
+        $color = "Red"
+    } else {
+        return
     }
-    elseif ($condition -eq "disable") {
-        Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Windows-Subsystem-Linux
-        Disable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform
-    }
+
+    Start-Process -Wait -WindowStyle Hidden -Verb RunAs powershell.exe -Args "-executionpolicy bypass -command Set-Location \`"$PWD\`"; $command"
+
+    # WSL feature is $status after reboot.
+    Write-Host "WSL feature is " -NoNewline
+    Write-Host $status -ForegroundColor $color -NoNewline
+    Write-Host " after reboot."
 }
 
-function Goto-Repo {
-    Set-Location $( join-path $(ghq root) $( ghq list | peco ))
+function local:Invoke-GhqSetLocation {
+    $ghq_root = $(ghq root)
+    $repo = $(ghq list | fzf)
+    Set-Location $(join-path $ghq_root $repo)
 }
 
-Set-Alias -Name e   -Value explorer.exe
-Set-Alias -Name vi  -Value vim
-Set-Alias -Name wg  -Value winget
+Set-Alias -Name e -Value explorer.exe
 Set-Alias -Name tig -Value "C:\Program Files\Git\usr\bin\tig.exe"
-Set-Alias -Name gr   -Value Goto-Repo
+Set-Alias -Name wslfeature -Value Switch-WslFeature
+Set-Alias -Name repo -Value Invoke-GhqSetLocation
