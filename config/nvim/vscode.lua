473e714cced3d35d611cd66e3ac605d75b3a387a
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2023-06-21 19:16:59 +0900

    update vscode neovim keymap

--- config/nvim/vscode.lua
+++ config/nvim/vscode.lua
@@ -2,3 +2,4 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
+vim.api.nvim_set_keymap("n", "C-h", ":nohlsearch<CR>", { noremap = true, silent = true })
