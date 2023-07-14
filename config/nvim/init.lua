39b00930e7cfd775d4cf72efc1ec685975ed4829
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2023-07-14 21:58:47 +0900

    moved from vim-plug to lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,53 +2,63 @@
 -- Load Plugins --
 ------------------
 
--- vim-plug with Lua https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
-local plug = vim.fn["plug#"]
-vim.call("plug#begin")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- GUI enhancements
-plug("nvim-lualine/lualine.nvim")
-plug("arkav/lualine-lsp-progress")
-plug("folke/tokyonight.nvim", { branch = "main" })
-plug("rebelot/kanagawa.nvim")
-plug("machakann/vim-highlightedyank")
-plug("onsails/lspkind.nvim")
--- plug('andymass/vim-matchup')
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+require("lazy").setup({
+    -- GUI enhancements
+    "nvim-lualine/lualine.nvim",
+    "arkav/lualine-lsp-progress",
+    { "folke/tokyonight.nvim", branch = "main" },
+    "rebelot/kanagawa.nvim",
+    "machakann/vim-highlightedyank",
+    "onsails/lspkind.nvim",
+    -- "andymass/vim-matchup",
 
--- Semantic language support
-plug("neovim/nvim-lspconfig")
-plug("williamboman/mason.nvim")
-plug("williamboman/mason-lspconfig.nvim") -- alternative to nvim-lsp-installer
+    -- Semantic language support
+    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim", -- alternative to nvim-lsp-installer
 
--- Completion plugins
-plug("hrsh7th/nvim-cmp")
-plug("hrsh7th/cmp-nvim-lsp")
-plug("hrsh7th/cmp-buffer")
-plug("hrsh7th/cmp-path")
-plug("hrsh7th/cmp-cmdline")
-plug("hrsh7th/vim-vsnip") -- required as a nvim-cmp dependency, even if not using snippet
+    -- Completion plugins
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/vim-vsnip", -- required as a nvim-cmp dependency, even if not using snippet
 
--- Syntactic language support
-plug("rust-lang/rust.vim")
-plug("cespare/vim-toml", { branch = "main" })
-plug("ron-rs/ron.vim")
-plug("mfussenegger/nvim-jdtls") -- For Java
-plug("nvim-treesitter/nvim-treesitter")
+    -- Syntactic language support
+    "rust-lang/rust.vim",
+    { "cespare/vim-toml", branch = "main" },
+    "ron-rs/ron.vim",
+    "mfussenegger/nvim-jdtls", -- For Java
+    "nvim-treesitter/nvim-treesitter",
 
--- Fuzzy finder
-plug("nvim-lua/plenary.nvim")
-plug("nvim-telescope/telescope.nvim", { branch = "0.1.x" })
+    -- Fuzzy finder
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
 
--- Utilities
-plug("vim-denops/denops.vim")
-plug("kat0h/bufpreview.vim") -- Markdown preview
-plug("tpope/vim-surround")
-plug("folke/which-key.nvim")
-plug("airblade/vim-gitgutter")
-plug("mfussenegger/nvim-dap")
-plug("jose-elias-alvarez/null-ls.nvim")
-
-vim.call("plug#end")
+    -- Utilities
+    "vim-denops/denops.vim",
+    "kat0h/bufpreview.vim", -- Markdown preview
+    "tpope/vim-surround",
+    "folke/which-key.nvim",
+    "airblade/vim-gitgutter",
+    "mfussenegger/nvim-dap",
+    "jose-elias-alvarez/null-ls.nvim",
+})
 
 ---------------------
 -- Plugin Settings --
