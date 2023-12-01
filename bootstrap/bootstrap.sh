b3a75adbedaf12803d8ef0623ad97b2303e123e0
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2023-12-01 23:52:18 +0900

    update install script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,145 +3,6 @@
 set -o nounset # error when referencing undefined variable
 set -o errexit # exit when command fails
 
-# Install packages
-function main() {
-    case $OSTYPE in
-    linux*)
-        if [ -x "$(command -v dnf)" ]; then
-            # Install packages from dnf
-            dnf_install
-        else
-            error "Command dnf not found. Unsupported distribution."
-            exit 1
-        fi
-
-        # Build and Install Neovim from source
-        git clone https://github.com/neovim/neovim /tmp/neovim && cd /tmp/neovim
-        make CMAKE_BUILD_TYPE=Release
-        sudo make install
-        cd ~
-
-        # Install starship
-        sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
-
-        # Install Rust
-        curl --proto ='https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-        # Install Deno (Mainly used for Neovim plugins)
-        curl -fsSL https://deno.land/install.sh | sh
-        ;;
-
-    darwin*)
-        brew_install
-        ;;
-
-    *)
-        error "Unsupported platform."
-        exit 1
-        ;;
-
-    esac
-
-    info "Finished package installation."
-
-    # Clone dotfiles repo and create symlinks
-    git clone https://github.com/shiomiyan/dotfiles.git ~/dotfiles
-    create_symlinks
-    info "Symlinks to dotfiles has been created."
-
-    # Install Neovim Plugins
-    nvim -es -u ~/.config/nvim/init.lua -i NONE -c "PlugInstall" -c "qa"
-}
-
-function dnf_install() {
-    # Upgrade current packages
-    sudo dnf -y upgrade
-
-    # Install toolchains
-    sudo dnf -y install \
-        bat \
-        fd-find \
-        gh \
-        git \
-        zsh \
-        wget \
-        unzip \
-        tmux \
-        nodejs \
-        openssl-devel \
-        ripgrep \
-        zoxide
-
-    # Neovim build dependencies
-    sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip patch gettext curl
-
-    # Setup GUI applications if desktop display server exists
-    if command -v Xorg >/dev/null; then
-        # Setup Nvidia driver
-        sudo dnf install \
-            "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-            "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-        sudo dnf install akmod-nvidia
-
-        sudo dnf install \
-            fcitx5 fcitx5-mozc fcitx5-lua \
-            xclip
-
-        # Desktop applications
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-        flatpak install -y \
-            com.bitwarden.desktop \
-            com.discordapp.Discord \
-            com.spotify.Client
-    fi
-}
-
-function brew_install() {
-    brew tap delphinus/sfmono-square
-    brew install \
-        bat \
-        gh \
-        git \
-        zsh \
-        curl \
-        tmux \
-        tig \
-        starship \
-        exa \
-        ripgrep \
-        zoxide \
-        sfmono-square
-    # If failed to install Neovim, see https://github.com/neovim/neovim/issues/16217#issuecomment-959793388
-    brew install --HEAD neovim
-
-    # Install GUI applications
-    brew tap wez/wezterm
-    brew install --cask \
-        alacritty \
-        firefox \
-        google-japanese-ime \
-        karabiner-elements \
-        keyboardcleantool \
-        spotify \
-        wireshark
-    brew install wez/wezterm/wezterm-nightly
-}
-
-function create_symlinks() {
-    mkdir -p /tmp/dot_backup
-    [ -f ~/.zshrc ] && mv ~/.zshrc /tmp/dot_backup/
-    [ -f ~/.zshenv ] && mv ~/.zshenv /tmp/dot_backup/
-    # [ -f ~/.tmux.conf ] && mv ~/.tmux.conf /tmp/dot_backup/
-    [ -d ~/.config ] && mv ~/.config /tmp/dot_backup/
-
-    ln -sf ~/dotfiles/.zshenv ~
-    ln -sf ~/dotfiles/.tmux.conf ~
-    ln -sf ~/dotfiles/config ~
-
-    # 退避させた`.config`配下のファイルおよびディレクトリを戻す
-    cp -r -n /tmp/dot_backup/.config/* ~/.config/
-}
-
 BOLD="$(tput bold 2>/dev/null || printf "")"
 UNDERLINE="$(tput smul 2>/dev/null || printf "")"
 RED="$(tput setaf 1 2>/dev/null || printf "")"
@@ -159,10 +20,173 @@ function error() {
     printf '%s\n' "${BOLD}${RED} :: $*${NO_COLOUR}"
 }
 
-info "Start Installation."
+DOTFILES_INSTALLER_TMP="$HOME/.dotfiles_installer_tmp"
 
-# Start installation
+# Install packages
+function main() {
+    mkdir -p $DOTFILES_INSTALLER_TMP
+    mkdir -p ~/.config/
+
+    setup-common-utils
+    setup-starship
+    setup-neovim
+    setup-rust
+    setup-deno
+    setup-nodejs
+    setup-ripgrep
+    setup-bat
+    setup-zoxide
+    setup-espanso
+}
+
+function setup-common-utils() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install git curl wget zsh unzip tig
+    elif [ -x "$(command -v apt)" ]; then
+        sudo apt update
+        sudo apt -y upgrade
+        sudo apt -y install git curl wget zsh unzip tig ca-certificates gnupg
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf -y upgrade
+        sudo dnf -y install git curl wget zsh unzip tig
+    else
+        error 'Unsupported operating system.'
+    fi
+
+    git clone https://github.com/shiomiyan/dotfiles.git ~/dotfiles
+    ln -sf ~/dotfiles/.zshenv ~
+    ln -sf ~/dotfiles/config/zsh ~/.config/zsh
+    ln -sf ~/dotfiles/config/git ~/.config/git
+    ln -sf ~/dotfiles/config/tig ~/.config/tig
+}
+
+function setup-starship() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install starship
+    else
+        sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+    fi
+
+    ln -sf ~/dotfiles/config/starship.toml ~/.config/starship.toml
+}
+
+function setup-neovim() {
+    if [ "$(uname)" == "Darwin" ]; then
+        # If failed to install Neovim, see https://github.com/neovim/neovim/issues/16217#issuecomment-959793388
+        brew install --HEAD neovim
+    else
+        # ref: https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
+        pushd $DOTFILES_INSTALLER_TMP
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+        chmod u+x nvim.appimage
+        ./nvim.appimage --appimage-extract
+        sudo mv squashfs-root /
+        sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+        popd
+    fi
+
+    ln -sf ~/dotfiles/config/nvim ~/.config/nvim
+}
+
+function setup-rust() {
+    curl --proto ='https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+}
+
+function setup-deno() {
+    curl -fsSL https://deno.land/install.sh | sh
+}
+
+function setup-nodejs() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install node
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf -y install nodejs
+    elif [ -x "$(command -v apt)" ]; then
+        # Install NodeJS. ref: https://github.com/nodesource/distributions
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+        NODE_MAJOR=20
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+        sudo apt update && sudo apt -y install nodejs
+    fi
+}
+
+function setup-ripgrep() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install ripgrep
+    elif [ -x "$(command -v apt)" ]; then
+        pushd $DOTFILES_INSTALLER_TMP
+        curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+        sudo dpkg -i ripgrep_13.0.0_amd64.deb
+        popd
+    fi
+}
+
+function setup-bat() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install bat
+    elif [ -x "$(command -v apt)" ]; then
+        pushd $DOTFILES_INSTALLER_TMP
+        curl -LO https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb
+        sudo dpkg -i bat-musl_0.24.0_amd64.deb
+        popd
+    fi
+}
+
+function setup-zoxide() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install zoxide
+    else
+        curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    fi
+}
+
+function setup-espanso() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew tap espanso/espanso
+        brew install espanso
+    elif [ -x "$(command -v apt)" ]; then
+        pushd $DOTFILES_INSTALLER_TMP
+        wget https://github.com/federico-terzi/espanso/releases/download/v2.1.8/espanso-debian-x11-amd64.deb
+        sudo apt install ./espanso-debian-x11-amd64.deb
+        espanso service register
+        espanso start
+        popd
+    fi
+
+    ln -sf ~/dotfiles/config/espanso ~/.config/espanso
+}
+
+function setup-wezterm() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew install --cask wezterm
+    elif [ -x "$(command -v apt)" ]; then
+        pushd $DOTFILES_INSTALLER_TMP
+        curl -LO https://github.com/wez/wezterm/releases/download/nightly/wezterm-nightly.Debian12.deb
+        sudo apt install -y wezterm-nightly.Debian12.deb
+        popd
+    fi
+
+    ln -sf ~/dotfiles/config/wezterm ~/.config/wezterm
+}
+
+function setup-gui-applications() {
+    if [ "$(uname)" == "Darwin" ]; then
+        brew tap wez/wezterm
+        brew install --cask \
+            alacritty \
+            firefox \
+            google-japanese-ime \
+            karabiner-elements \
+            keyboardcleantool \
+            spotify \
+            wireshark
+        brew install wez/wezterm/wezterm-nightly
+    fi
+
+    # TODO: LinuxでGUIアプリをインストールするコマンドを書く
+}
+
 main
-
-info "Setup succeed."
-info "To update default shell, run: sudo usermod -s \`which zsh\` \$USER"
+info "Finished setup!"
+info "To update default shell, run command below:\nsudo usermod -s \`which zsh\` \$USER"
