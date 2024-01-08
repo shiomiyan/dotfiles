85b7b410f880661f1149fa16af516d9da3823d7f
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2024-01-08 22:40:11 +0900

    .

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -1,2 +1,7 @@
+-- Neovide config also in neovide/config.toml
+-- see: https://neovide.dev/config-file.html
+vim.o.guifont = "Fira Code,BIZ UDGothic,Symbols Nerd Font Mono:h10"
+vim.opt.linespace = 8
+vim.g.neovide_transparency = 0.8
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
 vim.g.neovide_cursor_animation_length = 0.01
