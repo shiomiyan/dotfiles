829b6443cd9929d9d513f937892c4fec882fc3f9
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2024-01-08 16:54:05 +0900

    fix path

--- bootstrap/helper/setup-obsidian.sh
+++ bootstrap/helper/setup-obsidian.sh
@@ -8,8 +8,9 @@ curl -L $BROWSER_DOWNLOAD_URL -o /opt/obsidian/Obsidian.AppImage
 chmod +x /opt/obsidian/Obsidian.AppImage
 
 ln -sf ~/dotfiles/local/share/applications/obsidian.desktop ~/.local/share/applications/obsidian.desktop
-ln -sf ~/dotfiles/config/obsidian/obsidian.png /opt/obsidian/obsidian.png
+ln -sf ~/dotfiles/local/share/icons/obsidian.png ~/.local/share/icons/obsidian.png
 
 chmod 755 ~/.local/share/applications/obsidian.desktop
 
 update-desktop-database ~/.local/share/applications/
+
