1602fa818ed091b8cc1b06b6c75ed5ee56f35823
shiomiyan <sk736b@protonmail.com>
2025-05-03 00:58:08 +0900

    remove unused code

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,24 @@
-# Command only works for Windows machine
+# load windows specific modules on windows
 if ($PSVersionTable.Platform -eq "Win32NT") {
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-
-    # Import custom script for Windows
     Import-Module Windows
 }
 
 Import-Module PSReadLine
 
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& {
-    (zoxide init --hook pwd powershell | Out-String)
-})
+# starship
+if (Get-Command starship -errorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+    $Env:STARSHIP_LOG = "error"
+}
 
-# Command completions
-try {
-    if (Get-Command gh -ErrorAction SilentlyContinue) {
-        Invoke-Expression -Command $(gh completion -s powershell | Out-String)
-    }
-} catch {}
+# zoxide
+
+if (Get-Command zoxide -errorAction SilentlyContinue) {
+    Invoke-Expression (& {
+        (zoxide init --hook pwd powershell | Out-String)
+    })
+}
 
 Set-PSReadlineOption -BellStyle None # No more beep
 Set-PSReadlineOption -HistoryNoDuplicates
@@ -27,5 +27,3 @@ Set-PSReadlineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete # Show Tab completion menu
 Set-PSReadLineKeyHandler -Key Ctrl+n -Function TabCompleteNext
 Set-PSReadLineKeyHandler -Key Ctrl+p -Function TabCompletePrevious
-
-$Env:STARSHIP_LOG = "error"
