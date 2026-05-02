24187b157e247090b7ae19bb46c7c47ef5dce228
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2026-05-02 11:40:36 +0900

    refactor

--- nix/modules/home/common.nix
+++ nix/modules/home/common.nix
@@ -5,62 +5,39 @@
 }:
 
 {
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-
+  home.packages = with pkgs; [
     # Commons
-    pkgs.git
-    pkgs.zsh
-    pkgs.ripgrep
-    pkgs.bat
-    pkgs.curl
-    pkgs.wget
-    pkgs.unzip
-    pkgs.tig
-    pkgs.tree
-    pkgs.gh
-    pkgs.ghq
-    pkgs.fzf
-    pkgs.jq
+    git
+    ripgrep
+    bat
+    curl
+    wget
+    unzip
+    tig
+    tree
+    gh
+    ghq
+    fzf
+    jq
 
     # Languages
-    pkgs.zig
-    pkgs.go
-    pkgs.clang
-    pkgs.nodejs
-    pkgs.pnpm
-    pkgs.rustup
+    zig
+    go
+    clang
+    nodejs
+    pnpm
+    rustup
 
     # Utilities
-    pkgs.ghalint
-    pkgs.pinact
-    pkgs.actionlint
-    pkgs.betterleaks
+    ghalint
+    pinact
+    actionlint
+    betterleaks
 
     # AI
-    pkgs.llm-agents.codex
-    pkgs.llm-agents.codex-acp
-    pkgs.llm-agents.gemini-cli
-
-    # Misc
-    pkgs.pure-prompt
+    llm-agents.codex
+    llm-agents.codex-acp
+    llm-agents.gemini-cli
   ];
 
   programs.neovim = {
@@ -101,7 +78,6 @@
 
   programs.zsh = {
     enable = true;
-    package = pkgs.zsh;
     dotDir = "${config.home.homeDirectory}/.config/zsh";
 
     enableCompletion = true;
@@ -155,22 +131,6 @@
     "zsh/rc.zsh".source = ../../../home/.config/zsh/rc.zsh;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/sk/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
     XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
