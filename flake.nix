02bea7951319dc98bbf79b5b6732db86e6eb3c83
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2026-05-02 11:53:00 +0900

    update workflows

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   description = "Home Manager configuration of sk";
 
   nixConfig = {
-    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-substituters = [
+      "https://cache.numtide.com"
+      "https://nix-community.cachix.org"
+    ];
     extra-trusted-public-keys = [
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
