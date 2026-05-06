7e736ff1f756c305bd2bc907558e26e227fb30d8
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2026-05-06 11:42:04 +0900

    nix fmt

--- nix/hosts/wsl/users/sk/home-configuration.nix
+++ nix/hosts/wsl/users/sk/home-configuration.nix
@@ -15,7 +15,7 @@
     "git/wsl.gitconfig".text = ''
       [credential]
         helper = /mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe
-      
+
       [gpg]
         program = /mnt/c/Program Files/GnuPG/bin/gpg.exe
     '';
